6ec3fbdb3b9f8c40cf6de5e1d7e07de2abe8335a
joirneto <joirneto@gmail.com>
2021-11-24 15:01:31 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Layout from './components/layout'
+import Layout from './components/layout';
 import { ThemeProvider, CssBaseline } from "@mui/material";
-import { theme } from './lib/theme'
+import { theme } from './lib/theme';
 
 ReactDOM.render(
   <>
